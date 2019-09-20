2dc1654079d4e7629cc5a83b0c6285f8deb9aa92
Nataliya R <n.rudenko@gmail.com>
2019-09-20 20:19:00 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,29 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+  return document.querySelector('input').value
+}
+
+function addNewElementAsLi() {
+  const ul = document.querySelector('ul');
+   const li = document.createElement('li');
+   li.innerHTML = retrieveEmployeeInformation();
+   ul.appendChild(li);
+  
+}
+
+function   addNewLiOnClick() {
+  document.querySelector('input[type="submit"]').addEventListener('click', event =>{
+    addNewElementAsLi();
+    document.querySelector('input').value = "";
+  })
+}
+
+function clearEmployeeListOnLinkClick() {
+  const link = document.querySelector('a');
+  link.addEventListener('click', event => {
+    document.querySelector('ul').innerHTML = "";
+  })
+}
